599d4b2d5f230a3783f6dd5d6d424ad0d543c766
Pongsatorn <pongsatorn.kanja@gmail.com>
2025-07-12 16:02:46 +0700

    Object Detection Model Feature

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# poc-model
-Model Training
+# PoC-model Initial Set Up
+- Download the packages using conda;&nbsp; conda env create -f environment.yml
+- Request the dataset from Tai.
+- run checkCUDA.py before training (If you're using an NVIDIA GPU, CUDA is recommended for faster performance)
+<br>
+<br>
+
+# Start PoC-model 
+- trainDetection.py is used for model fine-tuning from yolo11m
+    - change *data path* and *model path* to match your local path (yolov11n.pt is downloaded automatically just for AMP (Automatic Mixed Precision), it still use yolov11m.pt as your weight)
+    - feel free to experiment with different hyperparameters for optimization. (This setting is use for RTX4070 Laptop VRAM 8 GB)
+- If you want to train the model again, deleting labels.cache is recommend (both train and valid data)
--- trainDetection.py
+++ trainDetection.py
@@ -4,20 +4,21 @@ def train_yolo():
     model = YOLO("yolo11m.pt")  # Load pre-trained YOLO model
 
     model.train(
-        data="C:/Users/user/Documents/VScode/DemoModel/data_detect.yaml",  # Dataset path
+        data="C:/Users/user/Documents/VScode/poc-model/data-detect.yaml",  # Dataset path
         epochs=50,               # Increased epochs due to smaller dataset
-        batch=32,                # Keep batch size moderate for stability
+        batch=4,                 # Keep batch size moderate for stability
         device="cuda",           # Use GPU
         lr0=0.0008,              # Lower learning rate for fine-tuning
         lrf=0.0001,              # Final learning rate with decay
-        cos_lr=True,             # Smooth learning rate decay
         weight_decay=0.0007,     # Slightly increased to prevent overfitting
         patience=5,              # Stops training early if no improvement
-        optimizer="AdamW"
+        optimizer="AdamW",       # modern and stable optimizer
+        cos_lr=True,             # Smooth learning rate decay
+        amp=True
     )
 
     # Save the trained model
-    model.save("car-frontal_detection.pt")
+    model.save("car_frontal_detection.pt")
 
 if __name__ == '__main__':
     train_yolo()
